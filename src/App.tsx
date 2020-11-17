728e78d1e593a535b907b3dcd5b4621646ac61e0
Lucas Américo de Souza Martins <lucas.martins@dtidigital.com.br>
2020-11-17 11:25:01 -0300

    Primeiro Comit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 import './App.css';
 
-function App() {
+const App = () => {
+
+  const [listaReact, setListaReact] = useState();
+
+  useEffect(() => {
+    axios.get(`https://react-challenge-api.herokuapp.com/first-challenge/episodes`)
+      .then(res => {
+        const retornoLista = res.data;
+        setListaReact(retornoLista);
+      })
+
+  }, []);
+  console.log(listaReact);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <div> oi </div>
+  )
 }
+
 
 export default App;

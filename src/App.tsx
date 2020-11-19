63f4187800ae5bfdfbd423d9fb95068b1edc9380
Cristian <olivecr@contractor.rhimagnesita.com>
2020-11-19 17:35:27 -0300

    backstreetboyz

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,24 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import './App.css';
+import {Container, Titulo, Subtitulo, Tabela} from './styles';
 
 const App = () => {
 
   const [listaReact, setListaReact] = useState();
 
- /* const boxStyle = {margin: '20px', border: 'solid-2px-darkgray', padding: '20px', border-color: 'black'};
-  const h1Style = {textTransform: 'uppercase', fontSize: '30px'};*/
-  const projetoAcontecer = `O projeto é moderado pelo Silvério e Júlio Sena, porém qualquer um é livre para discordar e expor sua opinião. Qualque um que pode dar inicio a discussão e se for de interesse fazer até mesmo um hands-on ou uma aprensentação.
-  As gravações tem sido iniciadas pelo Robson Melo e salvas no (dtiflix - Colocar o link aqui).`;
+  const reactTexto = `Sobre:
+  Porque nós o usamos?
+  É um fato conhecido de todos que um leitor se distrairá com o conteúdo de texto legível de uma página quando estiver examinando sua diagramação. A vantagem de usar Lorem Ipsum é que ele tem uma distribuição normal de letras, ao contrário de "Conteúdo aqui, conteúdo aqui", fazendo com que ele tenha uma aparência similar a de um texto legível. Muitos softwares de publicação e editores de páginas na internet agora usam Lorem Ipsum como texto-modelo padrão, e uma rápida busca por 'lorem ipsum' mostra vários websites ainda em sua fase de construção. Várias versões novas surgiram ao longo dos anos, eventualmente por acidente, e às vezes de propósito (injetando humor, e coisas do gênero).
+  `;
+  const projetoAcontecer = `Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+   Cras sagittis, enim ac iaculis iaculis, nunc tellus fringilla enim, at feugiat eros nulla in est.
+    Nullam ac leo enim. Cras maximus massa imperdiet, lacinia elit nec, vestibulum tellus.
+     Nullam euismod diam mi, et porttitor sem eleifend sit amet. Nulla eget ultrices nisl.
+      Aliquam ornare ornare enim, condimentum feugiat massa maximus in.
+       Duis eleifend eget magna vitae tincidunt. Nam lobortis placerat mauris ac fermentum.
+        Nam eu ornare purus, non auctor urna. Phasellus nec blandit lacus, sed consectetur ex.
+         Quisque convallis felis mi, quis cursus odio lobortis sed.`;
 
   useEffect(() => {
     axios.get(`https://react-challenge-api.herokuapp.com/first-challenge/episodes`)
@@ -20,38 +29,35 @@ const App = () => {
   }, []);
 
   return (
-  <div style={{backgroundColor: '#282c34'}}>
-    <div style={{backgroundColor: 'white'}}>
-      <h1>React Daily Chat</h1>
-      <p>Inciado como um projeto pelo seus idealizadores, Júli Sena e Silverio Vale no final do mês de Outubro de 2020, a intenção era de ser um chat de troca de informações sobre assuntos abordados dentro do mundo React, como: melhores práticas, ferramentas, etc.
-      Logo no seu segundo encontro já não funcionou mais como um simles chat, pois a proporção que tomou foi muito grande, se comparada a um chat, pois a reunião teve em torno de 14 pessoas presentes e a média hoje já está em 20 pessoas.
-      Desde então os debates estão sendo gravados e estão disponíveis na lista abaixo.
-      É um tempo de crescimento mutuo e amadurecimento na área não só do React, mas em geral, podemos ver o agilismo e o designon think (double diamond) sendo praticados com muita clareza.
-
-        </p>
-      <h2>Assuntos do debate</h2>
-      <h3>Já debatidos</h3>
-      <ul>
-        <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</li>
+    <Container>
+      <div>
+        <Titulo>REACT DAILY CHAT</Titulo>
+  <span>{reactTexto}</span>
+        <hr></hr>
+        <Titulo style={{ }}>ASSUNTOS DO DEBATE</Titulo>
+        <Subtitulo>Já debatidos</Subtitulo>
+        <Tabela>
+          <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</li>
             Gravação:
             <li>Ut quis nibh eu nunc finibus condimentum.</li>
             Gravação:
             <li>Aenean finibus risus ac urna bibendum, ac tempor enim euismod.</li>
             Gravação:
-          </ul>
-      <h3>Próximos assuntos</h3>
-      <p>Obs.: Esses assuntos são temas que podem sofrer alterações devido ao desenvolvimento do debate, porém serão assuntos que provavelmente não deixarão de ser abordados.
-      sugerimos que acompanhem todos os assuntos e já aboradados anteriormente para que assim aproveite melhor o conteúdo aqui explorado.
-      </p>
-      <ul>
-        <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</li>
-        <li>Ut quis nibh eu nunc finibus condimentum.</li>
-        <li>Aenean finibus risus ac urna bibendum, ac tempor enim euismod.</li>
-      </ul>
-      <h3>Quem faz o projeto acontecer?</h3>
-      <p>{projetoAcontecer}</p>
-    </div>
-  </div>
-  )}
+          </Tabela>
+        <Subtitulo>Próximos assuntos</Subtitulo>
+        <span>Obs.: Esses assuntos são temas que podem sofrer alterações devido ao desenvolvimento do debate, porém serão assuntos que provavelmente não deixarão de ser abordados.
+        sugerimos que acompanhem todos os assuntos e já aboradados anteriormente para que assim aproveite melhor o conteúdo aqui explorado.
+      </span>
+        <ul>
+          <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</li>
+          <li>Ut quis nibh eu nunc finibus condimentum.</li>
+          <li>Aenean finibus risus ac urna bibendum, ac tempor enim euismod.</li>
+        </ul>
+        <Subtitulo>Quem faz o projeto acontecer?</Subtitulo>
+        <span>{projetoAcontecer}</span>
+      </div>
+    </Container>
+  )
+}
 
 export default App;
---
+++ src/styles.ts
@@ -0,0 +1,21 @@
+import  styled from 'styled-components'
+//import ​​ from 'styled-components/cssprop'
+
+export const Container = styled.div`
+    background-color: #282c34;
+    margin: 60px;
+    color: white;
+`;
+
+export const Titulo = styled.h2`
+margin: 28px 0 20px;
+`;
+
+export const Subtitulo = styled(Titulo)`
+    font-size: 22px;
+`;
+
+export const Tabela = styled.ul`
+    text-decoration: underline;
+    color: #61dafb;
+`;

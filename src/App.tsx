a02b5e69c4cb5afd8a104d25319e97910b781506
Lucas Américo <53802824+LucasAmerico@users.noreply.github.com>
2020-11-20 12:52:05 -0300

    Ajustes gerais e configuracao de lista

--- src/App.tsx
+++ src/App.tsx
@@ -9,46 +9,51 @@ import {
   Box,
   Divisor
 } from './styles';
+import { isTypeNode } from 'typescript';
+
+interface InformacoesVideos {
+  id: number,
+  descricao: string,
+  data: string,
+}
 
 const App = () => {
 
-  const [listaReact, setListaReact] = useState();
+  const [listaReact, setListaReact] = useState<Array<InformacoesVideos>>([]);
 
-  const reactTexto = `Inciado como um projeto pelo seus idealizadores, Julio Sena e Silverio Vale no final do mês de Outubro de 2020, a intenção era de ser um chat de troca de informações sobre assuntos abordados dentro do mundo React, como: melhores práticas, ferramentas, etc.
-  Logo no seu segundo encontro já não funcionou mais como um simles chat, pois a proporção que tomou foi muito grande, se comparada a um chat, pois a reunião teve em torno de 14 pessoas presentes e a média hoje já está em 20 pessoas.  Desde então os debates estão sendo gravados e estão disponíveis na lista abaixo.
-  É um tempo de crescimento mutuo e amadurecimento na área não só do React, mas em geral, podemos ver o agilismo e o design thinking (double diamond) sendo praticados com muita clareza.`;
+  const reactTexto = <span>Iniciado como um projeto pelo seus idealizadores, Julio Sena e Silverio Vale no final do mês de Outubro de 2020, a intenção era de ser um chat de troca de informações sobre assuntos abordados dentro do mundo React, como: melhores práticas, ferramentas, etc.
+  Logo no seu segundo encontro já não funcionou mais como um simples chat, pois a proporção que tomou foi muito grande, se comparada a um chat, pois a reunião teve em torno de 14 pessoas presentes e a média hoje já está em 20 pessoas.  Desde então os debates estão sendo gravados e estão disponíveis na lista abaixo.
+É um tempo de crescimento mutuo e amadurecimento na área não só do React, mas em geral, podemos ver o agilismo e o design thinking (<a style={{ color: "#61dafb" }} href="https://medium.com/aela/double-diamond-como-utilizar-essa-metodologia-na-pr%C3%A1tica-5dc8a5d878bb">Double Diamond</a>) sendo praticados com muita clareza.</span>;
 
   const projetoAcontecer = `O projeto é moderado pelo Silvério e Julio Sena, porém qualquer um é livre para discordar e expor sua opinião. Qualquer um pode dar inicio a discussão e se for de interesse fazer até mesmo um hands-on ou uma aprensentação.
-  As reuniões tem sido gravadas e disponíveis no (dtiflix - Colocar o link aqui)`;
+  As reuniões tem sido gravadas e estão disponíveis no (dtiflix - Colocar o link aqui)`;
 
   useEffect(() => {
     axios.get(`https://react-challenge-api.herokuapp.com/first-challenge/episodes`)
       .then((res: any) => {
+
         const retornoLista = res.data;
         setListaReact(retornoLista);
       })
   }, []);
-
+  console.log(listaReact);
   return (
     <Container>
       <div>
         <Titulo>REACT DAILY CHAT</Titulo>
         <Box>
-          <span>{reactTexto}</span>
+          {reactTexto}
         </Box>
 
         <Divisor></Divisor>
 
-        <Titulo style={{}}>ASSUNTOS DO DEBATE</Titulo>
+        <Titulo>ASSUNTOS DO DEBATE</Titulo>
         <Subtitulo>Já debatidos</Subtitulo>
-        <Tabela>
-          <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</li>
-            Gravação:
-            <li>Ut quis nibh eu nunc finibus condimentum.</li>
-            Gravação:
-            <li>Aenean finibus risus ac urna bibendum, ac tempor enim euismod.</li>
-            Gravação:
-          </Tabela>
+        <Tabela> {listaReact &&
+          listaReact.map(((item: InformacoesVideos, index: number) => (
+            <li key={item.id}>{item.data} - {item.descricao}</li>
+          )))}
+        </Tabela>
 
         <Divisor></Divisor>
 
